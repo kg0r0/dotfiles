667c93fff2e877f5c2bcd3f374b704cb995538a4
kg0r0 <kg0r0@yahoo.co.jp>
2019-04-16 22:53:02 +0900

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+set -x GOPATH /Users/kgoro/secpf/workspace
+set -x GOROOT /usr/local/opt/go/libexec
+set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home
+set PATH /usr/local/bin /Users/kgoro/.nodebrew/current/bin /Users/kgoro/secpf/workspace/bin $PATH
+
+# encoding
+set -x LANG ja_JP.UTF-8
